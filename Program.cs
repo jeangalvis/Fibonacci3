49566cdde4d7d25f63aff07024d67066ab7cd209
jeangalvis <campusjean2023@gmail.com>
2023-12-04 15:20:33 -0500

    fibonacci3

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Ingrese m: ");
+int valor = Convert.ToInt32(Console.ReadLine());
+int temporal1 = 0;
+int temporal2 = 1;
+string cadena = "0 1";
+for(int i = 1; i<valor-1; i++){
+    int suma = temporal1 + temporal2;
+    temporal1 = temporal2;
+    temporal2 = suma;
+    cadena += " " + Convert.ToString(temporal2);
+}
+Console.WriteLine("Los primeros numeros de Fibonacci son: ");
+Console.WriteLine(cadena);
